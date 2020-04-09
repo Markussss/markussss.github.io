23647a784933c31086526fdd6a93a4dfee59df5c
Markus Igeland <markusigeland@gmail.com>
2020-04-09 17:35:01 +0200

    Added check to see if script already exists

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 Array.from(document.querySelectorAll('a')).forEach(el => {
     let url = `https://${document.location.hostname}`;
 
-    let scriptLoader = `var el=document.createElement('script');el.src='${url}/build/${el.dataset.scriptName}.js';document.body.appendChild(el);`
+    let scriptLoader = `if(typeof ${el.dataset.scriptName} === "function"){${el.dataset.scriptName}();return;}var el=document.createElement('script');el.src='${url}/build/${el.dataset.scriptName}.js';document.body.appendChild(el);`
 
     el.href = `javascript:(()=>{${scriptLoader}})();`;
 });
